c2dcf2ba4cb53a0b659ab0abfc7f43f85a0e2a1e
Devin Pastoor <devin.pastoor@gmail.com>
2017-07-22 19:31:41 -0400

    function initializer bbq_client

--- R/Babylon.R
+++ R/Babylon.R
@@ -67,6 +67,8 @@ time_difference <- function(end_time, start_time, units = "secs") {
 #' # get all directories for completed runs
 #' bbq$get_models(status = "COMPLETED") %>% map_chr(~ .x$RunInfo$RunDir)
 #' }
+#' @docType class
+#' @rdname bbq_client
 #' @export
 Babylon <-
     R6::R6Class("Babylon",
@@ -187,6 +189,22 @@ Babylon <-
             address = NULL
         )
 )
+
+#' create a new client for babylon queue
+#' @return Babylon instance
+#' @param init whether to initialize bbq
+#' @param host hostname
+#' @param port port bbq listens on
+#' @param verbose more information about whats going on under the hood
+#' @param must_work must be able to ping a valid bbq server to initialize
+#' @export
+bbq_client <- function(init = NULL,
+                       host = "http://localhost",
+                       port = 3333,
+                       verbose = TRUE,
+                       must_work = TRUE) {
+    Babylon$new(init = init, host = host, port = port, verbose = verbose, must_work = must_work)
+}
 
 #bab <- Babylon$new()
 #tmpresp <- bab$get_models()
